4bb5f0c3dae5164b2931817bcf81669c4f2f1f61
chintank <chintank@vehant.com>
2025-08-23 22:24:31 +0530

    extract the error message component

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from 'react';
+
+import { Text } from '@radix-ui/themes';
+
+const ErrorMessage = ({ children }: PropsWithChildren) => {
+  return children ? (
+    <Text color="red" as="p">
+      {children}
+    </Text>
+  ) : null;
+};
+
+export default ErrorMessage;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,7 +9,9 @@ import { z } from 'zod';
 import { Button, Text, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
+
 import { createIssueSchema } from '../../validationSchemas';
+import ErrorMessage from '../../components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -42,11 +44,7 @@ const NewIssuePage = () => {
       <TextField.Root>
         <TextField.Input placeholder="Title" {...register('title')} />
       </TextField.Root>
-      {errors.title && (
-        <Text color="red" as="p">
-          {errors.title.message}
-        </Text>
-      )}
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
       <Controller
         name="description"
         control={control}
@@ -54,11 +52,7 @@ const NewIssuePage = () => {
           <SimpleMDE placeholder="Description" {...field} />
         )}
       />
-      {errors.description && (
-        <Text color="red" as="p">
-          {errors.description.message}
-        </Text>
-      )}
+      <ErrorMessage>{errors.description?.message}</ErrorMessage>
       <Button>Submit New Issue</Button>
     </form>
   );
