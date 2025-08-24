cfaf6bcc920df8a80d396ffea9ec3e0327192154
chintank <chintank@vehant.com>
2025-08-24 18:45:24 +0530

    improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { notFound } from 'next/navigation';
 import prisma from '@/prisma/client';
-import IssueForm from '../../_components/IssueForm';
+
+import IssueFormSkeleton from '../../_components/IssueFormSkeleton';
+
+const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
   params: { id: string };
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,16 +1,3 @@
-import React from 'react';
+import IssueFormSkeleton from '../_components/IssueFormSkeleton';
 
-import { Box } from '@radix-ui/themes';
-
-import { Skeleton } from '@/app/components';
-
-const LoadingNewIssuePage = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton />
-      <Skeleton height="20rem" />
-    </Box>
-  );
-};
-
-export default LoadingNewIssuePage;
+export default IssueFormSkeleton;
