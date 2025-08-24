7c92621ba85f7055a556ad5878116b5f77dedd01
chintank <chintank@vehant.com>
2025-08-24 14:31:44 +0530

    refactor: organize import statements

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import { Table } from '@radix-ui/themes';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
 
 import IssueActions from './IssueActions';
+import { Skeleton } from '@/app/components';
 
 const LoadingIssuesPage = () => {
   const issues = [1, 2, 3, 4, 5];
