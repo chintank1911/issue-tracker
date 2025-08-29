abb232794aa050184a0fdf2ef341f4c6add6476b
chintank <chintank@vehant.com>
2025-08-29 17:31:53 +0530

    build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import { Pagination } from './components';
+import LatestIssues from './LatestIssues';
 
-interface Props {
-  searchParams: { page: string };
-}
-
-export default function Home({ searchParams }: Props) {
-  const page = parseInt(searchParams.page) || 1;
-
-  return <Pagination itemCount={100} pageSize={10} currentPage={page} />;
+export default function Home() {
+  return <LatestIssues />;
 }
