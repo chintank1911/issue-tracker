28777f2b0cb3b5d2359934e632de0c3139c32eec
chintank <chintank@vehant.com>
2025-08-29 21:43:06 +0530

    set up error tracking

---
+++ instrumentation-client.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://84f4d1e5bdf2cc7685f644a134be0e44@o4509928197586944.ingest.us.sentry.io/4509928206893061",
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
